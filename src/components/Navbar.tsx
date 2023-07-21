dde0e04d38f7060a816c42c1f126fca35eae663a
teojiesern <teojiesern42@gmail.com>
2023-07-21 22:03:25 +0800

    done create community ui

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ export default async function Navbar() {
     const session = await getAuthSession();
 
     return (
-        <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-10 py-2">
+        <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-10 py-5">
             <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2 ">
                 <Link href="/" className="flex gap-2 items-center">
                     <Icons.logo className="h-8 w-8 sm:h-10 sm:w-10" />
